7381e6343c3cfec9f4dfaf780aafc41a3f301b9b
Sahithya223 <sahithyaparnapalli@gmail.com>
2025-05-21 18:33:13 +0530

    third commit

--- src/main/java/com/example/demo/PracticeApplication.java
+++ src/main/java/com/example/demo/PracticeApplication.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeApplication {
+	static Logger logger=LoggerFactory.getLogger(PracticeApplicationController.class);
 
 	public static void main(String[] args) {
+		logger.info("application started");
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
